de001bcea3d1d4e6057b0febc995644549626dcb
Doug-Woog <marcus.d.lowe@gmail.com>
2019-07-04 17:37:34 +0100

    Added Relavent Files

---
+++ Object-Detection-Metrics-master/Model Testing.py
@@ -0,0 +1,186 @@
+import cv2
+import os
+import numpy as np
+from imutils.video import FPS
+import sys
+import csv
+from PeopleDetectionMetrics import calculate_metrics
+data_root = 'C:\WEX with Ben\PETS Data'
+sys.path.insert(0, data_root)
+sys.path.insert(0,"C:\\Users\Daniel\\bgslibrary\\bgslibrary")
+#sys.path.insert(0,'C:\\Users\\Daniel\\bgslibrary\\bgslibrary\\build')
+from MaskPostProcessing import MaskPostProcessing
+from BoundingBoxUtilities import BoundingBoxUtilities as bbox_utils
+from VisualizationTools import VisualizationTools as visualize
+import bgs
+INPUT_FRAME_SHAPE = (270, 360, 3)
+INPUT_FRAME_W = 360
+INPUT_FRAME_H = 270
+
+def pretrain_on_background_vid(fgbg,PRETRAINING_NUMBER_OF_FRAMES,display=True,mog=False):
+    initial_background_vid = cv2.VideoCapture(os.path.join(data_root,'S0_BG_View_001_Time_13-32-bestmatch.mp4'))
+
+    # Pretrain on 100 frames
+    frame_num = 0
+    while(True):
+        # Capture frame-by-frame
+        ret, img = initial_background_vid.read()
+        
+       
+        # This is the safe way to exit loop if end of video/problem with frame
+        if (not ret) or (img is None):
+            break
+        
+        if img.shape != INPUT_FRAME_SHAPE:
+            img = cv2.resize(img, (INPUT_FRAME_W, INPUT_FRAME_H))
+            assert img.shape == INPUT_FRAME_SHAPE
+        
+        if mog == True:
+            fgbg.apply(img, learningRate = 0.001)
+        else:
+            fgbg.apply(img)
+        
+        if display == True:
+            cv2.imshow('img', img)
+        
+            k = cv2.waitKey(1)
+        
+            if k == ord('q'):
+                break
+        
+        frame_num += 1
+        
+        if frame_num == PRETRAINING_NUMBER_OF_FRAMES:
+            break
+        
+    initial_background_vid.release()
+    cv2.destroyAllWindows()    
+    return fgbg
+
+def train_model(model,write=False,display=True,mog=False):
+    foreground_vid = cv2.VideoCapture(os.path.join(data_root, 'S2_L1_Time_12-34_View_001_2fps.mp4'))
+    frame_num = 0
+    fps_manager = FPS().start() # start timing FPS
+
+    while(True):
+        # Capture frame-by-frame
+        ret, img = foreground_vid.read()
+        
+        # This is the safe way to exit loop if end of video/problem with frame
+        if (not ret) or (img is None):
+            break
+    
+        if img.shape != INPUT_FRAME_SHAPE:
+            img = cv2.resize(img, (INPUT_FRAME_W, INPUT_FRAME_H))
+            assert img.shape == INPUT_FRAME_SHAPE
+        
+
+        if mog == True:
+            fgmask = model.apply(img, learningRate = 0.0005)
+            #fgmask2 = model2.apply(img)
+        else:
+            fgmask = model.apply(img)
+        
+        fgmask_enhanced = MaskPostProcessing.apply_filter('filter3_low_recall_low_noise_entrance', fgmask)
+        fgmask_enhanced, boxes = MaskPostProcessing.findContours(fgmask_enhanced, lower_area=0, upper_area=np.inf)
+        """
+        fgmask_enhanced2 = MaskPostProcessing.apply_filter('filter3_low_recall_low_noise_entrance', fgmask2)
+        fgmask_enhanced2, boxes2 = MaskPostProcessing.findContours(fgmask_enhanced2, lower_area=0, upper_area=np.inf)
+        """
+        if write == True:
+            with open("C:\\WEX with Ben\PETS Data\Object-Detection-Metrics-master\detections\\frame"+str(frame_num)+".txt",'w') as f:
+                for box in boxes:
+                    f.write("Person "+'1 '+str(box[0])+' '+str(box[1])+' '+str(box[2])+' '+str(box[3])+'\n')
+
+        if display == True:
+            #img2 = copy.deepcopy(img)
+            cv2.imshow('Input Image', img)
+            cv2.imshow('Output Mask', fgmask)
+            cv2.imshow('Enhanced Mask', fgmask_enhanced)
+            if mog==False:
+                cv2.imshow('Background',model.getBackgroundModel())
+            
+            visualize.drawBoxes(img, boxes)
+            cv2.imshow('Boxed Mask', img)
+            """
+            cv2.imshow('Output Mask Mixture', fgmask2)
+            cv2.imshow('Enhanced Mask Mixture', fgmask_enhanced2)
+            #cv2.imshow('Background Mixture',model2.getBackgroundModel())
+            
+            visualize.drawBoxes(img2, boxes2)
+            cv2.imshow('Boxed Mask Mixture', img2)
+            """
+            k = cv2.waitKey()
+    
+            if k == ord('q'):
+                cv2.waitKey(1)
+                #break
+    
+        fps_manager.update()
+        frame_num+=1
+ 
+    foreground_vid.release()
+    cv2.destroyAllWindows()
+
+    # Measure performance
+    fps_manager.stop()
+    time_elapsed = fps_manager.elapsed()
+    print("[STDOUT] elapsed time: {:.2f}".format(time_elapsed))
+    print("[STDOUT] approx. FPS: {:.2f}".format(fps_manager.fps()))
+    return fps_manager.fps()
+
+#import copy
+
+def test(models,IOU,WithMOG=True,AP=False):
+    
+    with open(models,'r') as a:
+        results = dict()
+        results["Headers"] = []
+        results["Headers"].append("FPS")
+        for thresh in IOU:
+            if AP == True:
+                results["Headers"].append("AP (IOU = "+str(thresh)+")")
+            results["Headers"].append("Precision (IOU = "+str(thresh)+")")
+            results["Headers"].append("Recall (IOU = "+str(thresh)+")")
+            
+        for model in a.readline()[1:-1].split("', '"): #Removed T2FMRF_UM model
+            if model[:2] != "LB" and model[:5] != "Multi" and model != "VuMeter":
+                results[model] = []
+                model_test = getattr(bgs, model)() 
+                model_test = pretrain_on_background_vid(model_test,100,display=False)
+                results[model].append(train_model(model_test,write=True,display=True))
+                for threshold in IOU:
+                    if AP == True:
+                        for result in calculate_metrics(IOUThresh=threshold):
+                            results[model].append(result)
+                    else:
+                        for result in calculate_metrics(IOUThresh=threshold)[-2:]:
+                            results[model].append(result)
+                #print(results[model])
+    if WithMOG==True:
+        mog_model = cv2.createBackgroundSubtractorMOG2( history=200,
+                                           varThreshold=200,
+                                        detectShadows=True)
+        results["BackgroundSubtractorMOG2"] = []
+        model_test = pretrain_on_background_vid(mog_model,100,display=False,mog=True)
+        results["BackgroundSubtractorMOG2"].append(train_model(mog_model,write=True,display=False,mog=True))
+        for threshold in IOU:
+            for result in calculate_metrics(threshold):
+                results["BackgroundSubtractorMOG2"].append(result)
+
+    with open("AAATest_Results IOU Threshold ="+str(IOU)+"AP="+str(AP)+".csv", "w",newline='') as outfile:
+       writer = csv.writer(outfile)
+       writer.writerow(results.keys())
+       writer.writerows(zip(*results.values()))
+
+test("TEst Models.txt",[0.25,0.5,0.75],AP=False,WithMOG=False) #Available bgs models.txt
+
+"""
+mog_model = cv2.createBackgroundSubtractorMOG2( history=200,
+                                                   varThreshold=200,
+                                                detectShadows=True)
+        mixture_model = bgs.MixtureOfGaussianV2()
+        pretrain_on_background_vid(mog_model,100,display=False,mog=True)
+        pretrain_on_background_vid(mixture_model,100,display=False)
+        train_model(mog_model,mixture_model,write=False,display=True,mog=True)
+"""
---
+++ Object-Detection-Metrics-master/PeopleDetectionMetrics.py
@@ -0,0 +1,186 @@
+
+import _init_paths
+from BoundingBox import BoundingBox
+from BoundingBoxes import BoundingBoxes
+from Evaluator import *
+import numpy
+import os
+def getBoundingBoxes():
+    """Read txt files containing bounding boxes (ground truth and detections)."""
+    allBoundingBoxes = BoundingBoxes()
+    import glob
+    import os
+    # Read ground truths
+    currentPath = os.path.dirname(os.path.abspath(__file__))
+    folderGT = os.path.join(currentPath, 'groundtruths')
+    os.chdir(folderGT)
+    files = glob.glob("*.txt")
+    files.sort()
+    # Class representing bounding boxes (ground truths and detections)
+    allBoundingBoxes = BoundingBoxes()
+    groundtruthboxes = dict()
+    detectionboxes = dict()
+    # Read GT detections from txt file
+    # Each line of the files in the groundtruths folder represents a ground truth bounding box
+    # (bounding boxes that a detector should detect)
+    # Each value of each line is  "class_id, x, y, width, height" respectively
+    # Class_id represents the class of the bounding box
+    # x, y represents the most top-left coordinates of the bounding box
+    # x2, y2 represents the most bottom-right coordinates of the bounding box
+    for frame_num, f in enumerate(files):
+        if frame_num < 20:
+            continue
+        nameOfImage = f.replace(".txt", "")
+        groundtruthboxes[nameOfImage] = BoundingBoxes()
+        fh1 = open(f, "r")
+        for line in fh1:
+            line = line.replace("\n", "")
+            if line.replace(' ', '') == '':
+                continue
+            splitLine = line.split(" ")
+            idClass = splitLine[0]  # class
+            x = float(splitLine[1])  # confidence
+            y = float(splitLine[2])
+            w = float(splitLine[3])
+            h = float(splitLine[4])
+            bb = BoundingBox(
+                nameOfImage,
+                idClass,
+                x,
+                y,
+                w,
+                h,
+                CoordinatesType.Absolute, (360, 270),
+                BBType.GroundTruth,
+                format=BBFormat.XYWH)
+            allBoundingBoxes.addBoundingBox(bb)
+            groundtruthboxes[nameOfImage].addBoundingBox(bb)
+            """
+            bb2 = BoundingBox(
+                nameOfImage,
+                idClass,
+                x,
+                y,
+                w,
+                h,
+                CoordinatesType.Absolute, (360, 270),
+                BBType.Detected,
+                1,
+                format=BBFormat.XYWH)
+            allBoundingBoxes.addBoundingBox(bb2)
+            """
+            #detectionboxes[nameOfImage].addBoundingBox(bb2)
+        fh1.close()
+    # Read detections
+    folderDet = os.path.join(currentPath, 'detections')
+    os.chdir(folderDet)
+    files = glob.glob("*.txt")
+    files.sort()
+    # Read detections from txt file
+    # Each line of the files in the detections folder represents a detected bounding box.
+    # Each value of each line is  "class_id, confidence, x, y, width, height" respectively
+    # Class_id represents the class of the detected bounding box
+    # Confidence represents confidence (from 0 to 1) that this detection belongs to the class_id.
+    # x, y represents the most top-left coordinates of the bounding box
+    # x2, y2 represents the most bottom-right coordinates of the bounding box
+    
+    for frame_num, f in enumerate(files):
+        if frame_num < 20:
+            continue
+        # nameOfImage = f.replace("_det.txt","")
+        nameOfImage = f.replace(".txt", "")
+        detectionboxes[nameOfImage] = BoundingBoxes()
+        # Read detections from txt file
+        fh1 = open(f, "r")
+        for line in fh1:
+            line = line.replace("\n", "")
+            if line.replace(' ', '') == '':
+                continue
+            splitLine = line.split(" ")
+            idClass = splitLine[0]  # class
+            confidence = float(splitLine[1])  # confidence
+            x = float(splitLine[2])
+            y = float(splitLine[3])
+            w = float(splitLine[4])
+            h = float(splitLine[5])
+            bb = BoundingBox(
+                nameOfImage,
+                idClass,
+                x,
+                y,
+                w,
+                h,
+                CoordinatesType.Absolute, (360, 270),
+                BBType.Detected,
+                confidence,
+                format=BBFormat.XYWH)
+            allBoundingBoxes.addBoundingBox(bb)
+            detectionboxes[nameOfImage].addBoundingBox(bb)
+        fh1.close()
+    
+    os.chdir('..')
+    return allBoundingBoxes,groundtruthboxes,detectionboxes
+
+
+def createImages(dictGroundTruth, dictDetected):
+    """Create representative images with bounding boxes."""
+    import numpy as np
+    import cv2
+    # Define image size
+    width = 360
+    height = 270
+    # Loop through the dictionary with ground truth detections
+    data_root = 'C:\WEX with Ben\PETS Data'
+    foreground_vid = cv2.VideoCapture(os.path.join(data_root, 'S2_L1_Time_12-34_View_001_2fps.mp4'))
+    for key in range(1,397):
+
+        ret, img = foreground_vid.read()
+        # This is the safe way to exit loop if end of video/problem with frame
+        if (not ret) or (img is None):
+            break
+        """
+        if img.shape != (270, 360, 3):
+            img = cv2.resize(img, (INPUT_FRAME_W, INPUT_FRAME_H))
+            assert img.shape == (270, 360, 3)"""
+        #image = np.zeros((height, width, 3), np.uint8)
+        gt_boundingboxes = dictGroundTruth[("frame"+str(key))]
+        img = gt_boundingboxes.drawAllBoundingBoxes(img,("frame"+str(key)))
+        detection_boundingboxes = dictDetected[("frame"+str(key))]
+        img = detection_boundingboxes.drawAllBoundingBoxes(img,("frame"+str(key)))
+        # Show detection and its GT
+        cv2.imshow("All boxes", img)
+        cv2.waitKey()
+
+
+#boundingboxes,dictGroundTruth,dictDetected = getBoundingBoxes()
+# Uncomment the line below to generate images based on the bounding boxes
+#createImages(dictGroundTruth, dictDetected)
+# Create an evaluator object in order to obtain the metrics
+
+def calculate_metrics(IOUThresh = 0.5):
+    evaluator = Evaluator()
+
+    metricsPerClass = evaluator.GetPascalVOCMetrics(
+        getBoundingBoxes()[0],  # Object containing all bounding boxes (ground truths and detections)
+        IOUThreshold=IOUThresh,  # IOU threshold
+        method=MethodAveragePrecision.EveryPointInterpolation)  # As the official matlab code
+    #print("Average precision values per class:\n")
+    # Loop through classes to obtain their metrics
+
+    for mc in metricsPerClass:
+        # Get metric values per each class
+        c = mc['class']
+        precision = mc['precision']
+        recall = mc['recall']
+        average_precision = mc['AP']
+        ipre = mc['interpolated precision']
+        irec = mc['interpolated recall']
+        # Print AP per class
+        #print(len(precision))
+        print("Precision: "+str(precision[-1]))
+        print("Recall: "+str(recall[-1]))
+        return  average_precision, precision[-1], recall[-1],
+        #print('%s: %f' % (c, average_precision))
+        #print("\nRecall Average ="+str(numpy.mean(mc['recall'])))
+
+#calculate_metrics(IOUThresh = 0.5)
